f61b986e7d98e5809becd49d3be272ba4114f6ba
EricaMarroquin <ericamarroquin03@gmail.com>
2021-10-08 12:12:30 -0700

    add details view to stylist

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -33,5 +33,11 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Stylist chosenStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(chosenStylist);
+    }
   }
 }
